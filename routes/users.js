0aae0593737cab060fd2e856eb38cc5e548ad9fa
Kathy Lu <kaffy.lu@gmail.com>
2015-08-12 14:38:56 -0400

    added users page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find().exec().then(function(users){
+    res.render('users', { users: users });
+  }).catch(next);
 });
+
 
 module.exports = router;
