045ed18653c5c8a636b39aecc82637aeacb1b179
jl975 <jlee6891@gmail.com>
2015-08-12 10:34:30 -0400

    add models lol

---
+++ models/index.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+// Notice the `mongodb` protocol; Mongo is basically a kind of server,
+// which handles database requests and sends responses. It's async!
+mongoose.connect('mongodb://localhost/wikistack');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+
+var pageSchema = new mongoose.Schema({
+  title:    {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content:  {type: String, required: true},
+  status:   {type: String, enum: ['open', 'closed']},
+  date:     {type: Date, default: Date.now},
+  author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+pageSchema.virtual('route').get(function() {
+	return '/wiki/'+urlTitle;
+});
+
+var userSchema = new mongoose.Schema({
+  name: {
+  	first: {type: String, required: true}, 
+  	last: {type: String, required: true},
+  },
+  email: {type: String, required: true, unique: true}
+});
+
+var Page = mongoose.model('Page', pageSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  Page: Page,
+  User: User
+};
+
